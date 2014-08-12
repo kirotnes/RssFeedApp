a48cf01126c37bb2bf4c3c10130b5cd8bd100f03
Kjell Inge Rotnes <kirotnes@gmail.com>
2014-08-12 11:49:33 +0200

    changed port

--- app.js
+++ app.js
@@ -76,6 +76,6 @@ app.get("/getGuid", function(req, res){
 
 
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.port||3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
